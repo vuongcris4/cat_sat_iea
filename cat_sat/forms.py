7974412caaea88cd21b6d06d555b6df492c2e066
vuongcris4 <tranduyvuong100@gmail.com>
2025-03-04 10:19:24 +0700

    thêm đơn vị vô form

--- cat_sat/forms.py
+++ cat_sat/forms.py
@@ -6,7 +6,7 @@ class OptimizationForm(forms.Form):
 
     length = forms.ChoiceField(
         choices=LENGTH_CHOICES,
-        label="Chiều dài thanh sắt",
+        label="Chiều dài thanh sắt (mm)",
         initial=5850,
         widget=forms.Select(attrs={'class': 'form-control'})
     )
@@ -17,26 +17,26 @@ class OptimizationForm(forms.Form):
     #     self.fields['length'].widget.attrs['datalist'] = 'length-options'
 
     te_dau_sat = forms.IntegerField(
-        label="Tề đầu sắt",
+        label="Tề đầu sắt (mm)",
         initial=0,
         widget=forms.NumberInput(attrs={'class': 'form-control'})
     )
 
     segment_sizes = forms.CharField(
-        label="Kích thước đoạn",
+        label="Kích thước đoạn (mm)",
         initial="500 255 600 615",
         widget=forms.TextInput(attrs={'class': 'form-control'})
     )
 
     demands = forms.CharField(
-        label="Số lượng cần",
+        label="Số lượng cần (đoạn)",
         initial="782 1564 1508 1508",
         widget=forms.TextInput(attrs={'class': 'form-control'})
     )
 
     blade_width = forms.ChoiceField(
         choices=BLADE_WIDTH_CHOICES,
-        label="Độ dày lưỡi cắt",
+        label="Độ dày lưỡi cắt (mm)",
         initial=2.5,
         widget=forms.Select(attrs={'class': 'form-control'})
     )
@@ -48,13 +48,13 @@ class OptimizationForm(forms.Form):
     )
 
     max_manual_cuts = forms.IntegerField(
-        label="Số lần cắt thủ công tối đa",
+        label="Số lần cắt thủ công tối đa (lần)",
         initial=5,
         widget=forms.NumberInput(attrs={'class': 'form-control'})
     )
 
     max_stock_over = forms.IntegerField(
-        label="Phần dư mỗi đoạn tối đa",
+        label="Phần dư mỗi đoạn tối đa (đoạn)",
         initial=20,
         widget=forms.NumberInput(attrs={'class': 'form-control'})
     )
