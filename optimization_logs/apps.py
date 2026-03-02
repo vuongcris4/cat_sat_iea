af54279dc9d1b8987df94ad29223a4a3e7b13eac
Vuong Tran <tranduyvuong100@gmail.com>
2026-03-02 16:08:09 +0700

    feat: Phase 2 - optimization_logs Django app (model, admin)

---
+++ optimization_logs/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class OptimizationLogsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'optimization_logs'
+    verbose_name = 'Lịch sử Tối ưu hóa'
